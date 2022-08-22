75ece5e5522f056f8af7e4be8652f3d067444eb8
Supun Maduranga <86388269+szsupunma@users.noreply.github.com>
2022-08-22 00:00:55 -0700

    --config--

--- Rose/utils/commands.py
+++ Rose/utils/commands.py
@@ -1,6 +1,6 @@
 from pyrogram import filters
 from typing import List, Union
-from config import COMMAND_PREFIXES
+from config import Config
 
 def command(commands: Union[str, List[str]]):
-    return filters.command(commands, COMMAND_PREFIXES)
+    return filters.command(commands, Config.COMMAND_PREFIXES)
