33fe9a92093ef1611793267b98d68cef275bd161
Supun Maduranga <86388269+szsupunma@users.noreply.github.com>
2022-08-21 23:49:40 -0700

    --update--

--- config.py
+++ config.py
@@ -1,18 +1,29 @@
-from dotenv import load_dotenv
 from os import environ
 
-load_dotenv("config.env")
-
-BOT_TOKEN = environ.get("BOT_TOKEN")
-API_ID = int(environ.get("API_ID"))
-API_HASH = environ.get("API_HASH")
-API_ID1 = int(environ.get("API_ID1"))
-API_HASH1 = environ.get("API_HASH1")
-SUDO_USERS_ID = environ.get("SUDO_USERS_ID")
-LOG_GROUP_ID = environ.get("LOG_GROUP_ID")
-BASE_DB = environ.get("BASE_DB")
-MONGO_URL = environ.get("MONGO_URL")
-ARQ_API_URL = environ.get("ARQ_API_URL")
-ARQ_API_KEY = environ.get("ARQ_API_KEY")
-COMMAND_PREFIXES = environ.get("COMMAND_PREFIXES")
-F_SUB_CHANNEL = environ.get("F_SUB_CHANNEL")
+class Config(object):
+        #Your telegram BOT API token : get it from @BotFather
+        BOT_TOKEN = environ.get("BOT_TOKEN")
+        #API_ID of your Telegram Account my.telegram.org/apps
+        API_ID = int(environ.get("API_ID"))
+        #API_HASH of your Telegram Account my.telegram.org/apps
+        API_HASH = environ.get("API_HASH")
+        #API_ID of your Telegram Account my.telegram.org/apps
+        API_ID1 = int(environ.get("API_ID1"))
+        #API_HASH of your Telegram Account my.telegram.org/apps
+        API_HASH1 = environ.get("API_HASH1")
+        #Your telegram user id
+        OWNER_ID = environ.get("OWNER_ID")
+        #For logs channel to note down important bot level events, recommend to make this public. ex: '-123456'
+        LOG_GROUP_ID = environ.get("LOG_GROUP_ID")
+        #Get From Here.https://www.mongodb.com/ (Same as MONGO_URL but give differant value for this) 
+        BASE_DB = environ.get("BASE_DB")
+        #Get From Here.https://www.mongodb.com/
+        MONGO_URL = environ.get("MONGO_URL")
+        #Don't change this value:https://arq.hamker.in
+        ARQ_API_URL = environ.get("ARQ_API_URL")
+        #Get this from @ARQRobot.
+        ARQ_API_KEY = environ.get("ARQ_API_KEY")
+        #now you can set custom command handler for rose like : / ! ,
+        COMMAND_PREFIXES = environ.get("COMMAND_PREFIXES")
+        #The Telegram channel id you want focus user.(User can't start your bot without join it)
+        F_SUB_CHANNEL = environ.get("F_SUB_CHANNEL")
